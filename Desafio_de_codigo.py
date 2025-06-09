2a2f7357573aab62e38acdcb6295dbd2516feebb
Rodrigo Nascimento <rodrigowaltmann.n@gmail.com>
2025-06-09 17:11:53 -0300

    feat(banco): criar sistema bancário simples com depósito, saque, extrato e limite de saques

---
+++ Desafio_de_codigo.py
@@ -0,0 +1,15 @@
+# Dicionário com os valores de desconto
+descontos = {
+    "DESCONTO10": 0.10,
+    "DESCONTO20": 0.20,
+    "SEM_DESCONTO": 0.00
+}
+
+# Entrada do usuário
+preco = float(input().strip())
+cupom = input().strip()
+
+# TODO: Aplique o desconto se o cupom for válido:
+
+# if cupom in descontos:
+#   print("O cupom está na lista")
