94c42212ba3fdb3085ca9399e3bbafd48692fd94
Rodrigo Nascimento <rodrigowaltmann.n@gmail.com>
2025-06-09 18:39:59 -0300

    feat(banco): cria função exibir extrato e adiciona sua chamada dentro do while

--- Sistema_bancario.py
+++ Sistema_bancario.py
@@ -46,6 +46,13 @@ def sacar(valor, saldo, extrato, numero_saques, limite, LIMITE_SAQUES):
 
     return saldo, extrato, numero_saques
 
+def exibir_extrato(saldo, extrato, LIMITE_SAQUES, numero_saques): 
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print(f"Você ainda pode realizar {LIMITE_SAQUES - numero_saques} saque(s) hoje.")
+    print("==========================================")
+
 while True:
     opcao = input(menu)
 
@@ -58,12 +65,7 @@ while True:
         saldo, extrato, numero_saques = sacar(valor, saldo, extrato, numero_saques, limite, LIMITE_SAQUES)
 
     elif opcao == "e":
-        print("\n================ EXTRATO ================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"\nSaldo: R$ {saldo:.2f}")
-        print(f"Você ainda pode realizar {LIMITE_SAQUES - numero_saques} saque(s) hoje.")
-        print("==========================================")
-
+        exibir_extrato(saldo, extrato, LIMITE_SAQUES, numero_saques)
 
     elif opcao == "q":
         print("Programa encerrado. Até mais!")
